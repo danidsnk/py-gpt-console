4e8bb262999eb830397d6110a17b214e5b0d9f9e
danidsnk <mdesinka@gmail.com>
2023-06-27 23:30:09 +0600

    add commands wrapper

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class RichGptConsole:
-    def __init__(self, system_prompt='You are helpful assistant'):
+    def __init__(self, system_prompt: str = 'You are helpful assistant'):
         self.__system_prompt = {'role': 'system', 'content': system_prompt}
         self.__message_history = [self.__system_prompt]
 
@@ -28,7 +28,7 @@ class RichGptConsole:
                 return delta.content
         return ''
 
-    def __response_processing(self, prompt, live_rich):
+    def __response_processing(self, prompt: str, live_rich: Live):
         self.__message_history.append({'role': 'user', 'content': prompt})
         res = ''
         for response in self.__gpt_stream():
@@ -37,47 +37,66 @@ class RichGptConsole:
 
         self.__message_history.append({'role': 'assistant', 'content': res})
 
-    def rich_chat(self, prompt):
+    def rich_chat(self, prompt: str):
         with Live(refresh_per_second=12) as live:
             self.__response_processing(prompt, live)
 
     def clear_history(self):
         self.__message_history = [self.__system_prompt]
 
-    def set_system_prompt(self, system_prompt):
+    def set_system_prompt(self, system_prompt: str):
         self.__system_prompt['content'] = system_prompt
         self.clear_history()
 
-    def raw_last_response(self):
+    def raw_last_response(self) -> str:
         return self.__message_history[-1]['content']
+
+
+class _Commands:
+    def __init__(self, gpt_console: RichGptConsole):
+        self.__gpt: RichGptConsole = gpt_console
+
+    def __call__(self, command: str) -> bool:
+        match command:
+            case '!exit':
+                return False
+            case '!clear':
+                self.__gpt.clear_history()
+            case '!system':
+                sysprompt = input('[ System ]: ')
+                if '!multi':
+                    sysprompt = self.__multiline_input()
+                if sysprompt:
+                    self.__gpt.set_system_prompt(sysprompt)
+            case '!raw':
+                print(self.__gpt.raw_last_response())
+            case '!multi':
+                multiline = self.__multiline_input()
+                if multiline:
+                    self.__gpt.rich_chat(multiline)
+
+        return True
+
+    def __multiline_input(self) -> str:
+        lines = []
+        while True:
+            try:
+                line = input()
+            except EOFError:
+                break
+            lines.append(line)
+        return '\n'.join(lines)
 
 
 if __name__ == '__main__':
     gpt = RichGptConsole()
+    command = _Commands(gpt)
     while True:
         prompt = input('[ User ]: ')
         if prompt.startswith('!'):
-            if prompt == '!exit':
+            if not command(prompt):
                 break
-            elif prompt == '!clear':
-                gpt.clear_history()
-                continue
-            elif prompt == '!system':
-                system_prompt = input('[ System ]: ')
-                gpt.set_system_prompt(system_prompt)
-                continue
-            elif prompt == '!raw':
-                print(gpt.raw_last_response())
-                continue
-            elif prompt == '!multi':
-                multiline = []
-                while True:
-                    try:
-                        line = input()
-                    except EOFError:
-                        break
-                    multiline.append(line)
-                prompt = '\n'.join(multiline)
+            continue
         elif prompt == '':
             continue
         gpt.rich_chat(prompt)
